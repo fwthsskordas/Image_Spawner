411ac810707abe394f14935cee31dc4ccb3c39c1
fwthsskordas <fwthsskordas@gmail.com>
2023-10-19 03:03:10 +0300

    Final commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template class="container">
+  <Counterhaha/>
+</template>
+
+<script>
+
+import Counterhaha from './components/CounterLast.vue'
+export default {
+  name: 'App',
+  components: {
+    Counterhaha,
+  }
+}
+</script>
+
+<style scoped>
+.container{
+
+  gap: 50px;
+}
+</style>
---
+++ src/components/CounterLast.vue
@@ -0,0 +1,47 @@
+<template class="all">
+    <div class="container">
+        <button class="myBtn" @click="increment"> Counter </button>
+        <div class="emojis"> {{ arrayEmojis }}</div>
+    </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default{
+    props:{
+        emoji: { type: String, default: '😬' }
+    },
+    setup(props){
+        const counter = ref(0)
+        const increment = () => counter.value++
+        
+        const arrayEmojis = computed (() =>
+        Array.from(new Array(counter.value), () => props.emoji).join(''))
+
+        return{counter, increment, arrayEmojis}
+    }
+}
+</script>
+
+<style scoped>
+
+.all{
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+   
+}
+.container{ 
+    margin-left: 900px;
+    width: 200px;
+}
+
+.myBtn{
+    width: 200px;
+    background-color:antiquewhite;
+    border-radius: 5px;
+    cursor: pointer;
+    padding: 5px;
+}
+</style>
